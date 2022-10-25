cfa6058b9bd08c7beda11dae7ab9bf8616b785dc
Duy Nguyen <thanhduy5112020@gmail.com>
2022-10-26 05:04:48 +0700

    JWT Authorization Vertification

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -29,21 +29,22 @@ export const login = async (req, res, next) => {
         if (!user) return next(createError(404, "User not found!"))
 
         const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
-        if(!isPasswordCorrect) return next(createError(400, "Wrong password, try again !"))
+        if (!isPasswordCorrect) return next(createError(400, "Wrong password, try again !"))
 
 
         const token = jwt.sign(
-            {id: user._id, isAdmin: user.isAdmin}, 
+            { id: user._id, isAdmin: user.isAdmin },
             process.env.JWT
         )
 
         const { password, isAdmin, ...otherDetails } = user._doc
-        res
+        return res
             .cookie("access_token", token, {
                 httpOnly: true,
             })
             .status(200)
             .json({ ...otherDetails })
+
     }
     catch (err) {
         next(err)
