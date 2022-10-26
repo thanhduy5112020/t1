c916a963b354936db52019aea08184a65a5709bb
Duy Nguyen <thanhduy5112020@gmail.com>
2022-10-26 19:44:10 +0700

    room model

---
+++ api/models/Room.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const RoomSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    maxPeople: {
+        type: Number,
+        required: true,
+    },
+    desc: {
+        type: String,
+        required: true,
+    },
+    roomNumbers: [{ number: Number, unavailableDates: [{ type: Date }] }],
+}, { timestamps: true })
+
+// [
+//     {number: 101, unavailableDates: [01.05.2022, 02.05.2022]}
+//     {number: 102, unavailableDates: [01.05.2022, 02.05.2022]}
+//     {number: 103, unavailableDates: []}
+//     {number: 104, unavailableDates: []}
+//     {number: 105, unavailableDates: []}
+    
+// ]
+
+export default mongoose.model("Room", RoomSchema)
--- api/utils/verifyToken.js
+++ api/utils/verifyToken.js
@@ -3,7 +3,6 @@ import { createError } from "./error.js";
 
 export const verifyToken = (req, res, next) => {
     const token = req.cookies.access_token;
-    console.log(token)
     if (!token) {
         return next(createError(401, "You are not authenticated!"))
     }
